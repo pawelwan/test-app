24bcb4c85883fe155ee434d7e8466fbef7f3b456
Pawel Wandzel <pawelwan@gmail.com>
2018-01-10 21:13:16 +0100

    add balanced AST

--- src/main/scala/parcalc/ArithmeticParser.scala
+++ src/main/scala/parcalc/ArithmeticParser.scala
@@ -5,22 +5,22 @@ import scala.util.parsing.combinator._
 object ArithmeticParser extends JavaTokenParsers {
   def expr: Parser[Expr] =
     term ~! rep("+" ~! term | "-" ~! term) ^^ {
-      case term ~ terms => terms.foldLeft(term) { (x, y) =>
-        y match {
-          case "+" ~ fact => BinOp(Add, x, fact)
-          case "-" ~ fact => BinOp(Sub, x, fact)
+      case term ~ terms =>
+        val adds = terms map {
+          case "-" ~ t => BinOp(Sub, Num(0.0), t)
+          case _ ~ t => t
         }
-      }
+        makeBalancedAST(Add, term :: adds)
     }
 
   def term: Parser[Expr] =
     factor ~! rep("*" ~! factor | "/" ~! factor) ^^ {
-      case factor ~ factors => factors.foldLeft(factor) { (x, y) =>
-        y match {
-          case "*" ~ ter => BinOp(Mul, x, ter)
-          case "/" ~ ter => BinOp(Div, x, ter)
+      case factor ~ factors =>
+        val muls = factors map {
+          case "/" ~ f => BinOp(Div, Num(1.0), f)
+          case _ ~ f => f
         }
-      }
+        makeBalancedAST(Mul, factor :: muls)
     }
 
   def factor: Parser[Expr] = (
@@ -29,6 +29,14 @@ object ArithmeticParser extends JavaTokenParsers {
   )
 
   def parseExpr(expression: String): ParseResult[Expr] = parseAll(expr, expression)
+
+  private def makeBalancedAST(op: Operation, xs: List[Expr]): Expr = xs match {
+    case List(x) => x
+    case _ =>
+      val half = xs.length / 2
+      val (l, r) = xs splitAt half
+      BinOp(op, makeBalancedAST(op, l), makeBalancedAST(op, r))
+  }
 }
 
 sealed class Operation(val apply: (Double, Double) => Double)
